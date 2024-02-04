6ad482b86b57feac068277c0fbe9ba516e5aea32
Angel <jamtgoner@gmail.com>
2024-02-04 17:42:46 -0400

    Agregando la v4 que utiliza un archivo json en vez de una base de datos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import paisRoutes from "./routes/pais-mysql.routes.js"
 import countriesRoutes from "./routes/country-pg.routes.js"
+import countriesRoutesJs from "./routes/country-json.routes.js"
 import {PORT} from "./config.js"
 
 const app = express()
@@ -13,6 +14,7 @@ app.use(express.json())
 
 app.use("/v1",paisRoutes)
 app.use("/v2",countriesRoutes)
+app.use("/v3",countriesRoutesJs)
 
 app.use((_req, res, next) => {
     res.status(404).json({ error: "Endpoint incorrecto, favor validar la documentación" })
---
+++ src/routes/country-json.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+ import {getPaises,getPais, getRandom, getPaisDiv, getPaisesIdioma } from "../controllers/countries-json.controller.js"
+
+
+const router = Router()
+
+router.get("/paises", getPaises )
+router.get("/pais/:id", getPais)
+router.get("/random", getRandom)
+router.get("/divisa/:codDiv", getPaisDiv)
+router.get("/idioma/:idioma", getPaisesIdioma)
+
+export default router
